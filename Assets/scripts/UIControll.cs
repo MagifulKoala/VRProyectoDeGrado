05a69bb0fa0a0081398e793c9469ab76968156aa
MagifulKoala <p.dorado@uniandes.edu.co>
2023-09-05 20:33:35 -0500

    added functional simple Door

--- Assets/scripts/UIControll.cs
+++ Assets/scripts/UIControll.cs
@@ -9,48 +9,55 @@ using System;
 public class UIControll : MonoBehaviour
 {
 
-    [SerializeField] string[] strings; 
+    [SerializeField] string[] strings;
     [SerializeField] TMP_Text[] textFields;
-    [SerializeField]  ButtonInteractible xrButton; 
-    [SerializeField] GameObject keyPointLight; 
+    [SerializeField] ButtonInteractible xrButton;
+    [SerializeField] GameObject keyPointLight;
 
 
-    
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
-        for (int i = 0; i < textFields.Length; i++)
+        if (textFields != null)
         {
-            textFields[i].text = strings[i];
+            for (int i = 0; i < textFields.Length; i++)
+            {
+                textFields[i].text = strings[i];
+            }
         }
 
-        if(xrButton != null)
+
+        if (xrButton != null)
         {
-            xrButton.onSelectEntered.AddListener(buttonPressed); 
+            xrButton.onSelectEntered.AddListener(buttonPressed);
         }
-        
+
     }
 
     int current = 1;
 
     private void buttonPressed(XRBaseInteractor arg0)
     {
-        keyPointLight.SetActive(true); 
-
-        Debug.Log("before if current: " + current); 
-        if(current > strings.Length - 1 )
+        if(keyPointLight != null)
         {
-            Debug.Log("enters if statment"); 
-            current = 1; 
+            keyPointLight.SetActive(true);
+        }
+        
+
+        Debug.Log("before if current: " + current);
+        if (current > strings.Length - 1)
+        {
+            Debug.Log("enters if statment");
+            current = 1;
         }
         textFields[0].text = strings[current];
-        current += 1; 
-        Debug.Log(current); 
-         
-        
+        current += 1;
+        Debug.Log(current);
+
+
     }
 
-    
+
 }
