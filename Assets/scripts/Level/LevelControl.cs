a8f16d8283e333f704bbf6bed4c0d35ab08a5c7c
MagifulKoala <p.dorado@uniandes.edu.co>
2023-09-20 19:51:47 -0500

    bugfixing and level design basic level
    added lighting, adjusted skybox, bug fixed some errors in level, added fog wall, fixed bug with spawnPoints

--- Assets/scripts/Level/LevelControl.cs
+++ Assets/scripts/Level/LevelControl.cs
@@ -8,29 +8,40 @@ public class LevelControl : MonoBehaviour
 {
     [SerializeField] Transform spawnPoint;
     [SerializeField] GameObject xrOrigin;
-   UnityEvent enableLeftHandPowers;
+    UnityEvent enableLeftHandPowers;
 
 
     private void Start()
     {
-        xrOrigin.transform.position = spawnPoint.position; 
-        xrOrigin.transform.localEulerAngles = spawnPoint.localEulerAngles;     
+        xrOrigin.transform.position = spawnPoint.position;
+        xrOrigin.transform.localEulerAngles = spawnPoint.localEulerAngles;
     }
 
 
     public void reloadLevel()
     {
-        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex; 
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentSceneIndex);
 
     }
 
+
+    public void resetPlayer()
+    {
+        Debug.Log("player pos:" + xrOrigin.transform.position);
+        Debug.Log("respawn pos:" + spawnPoint.transform.position);
+        xrOrigin.transform.position = spawnPoint.transform.position;
+        xrOrigin.transform.localEulerAngles = spawnPoint.transform.localEulerAngles;
+ 
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if(other.tag.Equals("worldBarrier"))
+        if (other.tag.Equals("worldBarrier"))
         {
+            //resetPlayer();
             reloadLevel();
-        }    
+        }
     }
 
 
